e6c8f67d69a52b77e0e6de1499c0e92316165bac
o.bayoumi <o.bayoumi@reply.it>
2024-06-14 10:09:38 +0200

    Backend created

---
+++ pepper.hmd/src/main/java/it/visionlab/sapienza/pepper/hmd/PepperServerHmdApplication.java
@@ -0,0 +1,13 @@
+package it.visionlab.sapienza.pepper.hmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepperServerHmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepperServerHmdApplication.class, args);
+	}
+
+}
---
+++ pepper.hmd/src/test/java/it/visionlab/sapienza/pepper/hmd/PepperServerHmdApplicationTests.java
@@ -0,0 +1,13 @@
+package it.visionlab.sapienza.pepper.hmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepperServerHmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
